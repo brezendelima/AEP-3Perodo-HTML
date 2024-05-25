2677a1a4c791cba96eb36286699a69c413d6f952
eduardoranucci <eduardoranucci@outlook.com>
2024-05-25 17:03:08 -0300

    login/register

---
+++ assets/js/script.js
@@ -0,0 +1,127 @@
+// Inputs cadastro
+const inputNome = document.getElementById('inputNome')
+const inputSobrenome = document.getElementById('inputSobrenome')
+const inputCPF = document.getElementById('inputCPF')
+const inputCNPJ = document.getElementById('inputCNPJ')
+const inputEmailCadastro = document.getElementById('inputEmailCadastro')
+const inputSenhaCadastro = document.getElementById('inputSenhaCadastro')
+
+// Inputs login
+const inputEmailLogin = document.getElementById('inputEmailLogin')
+const inputSenhaLogin = document.getElementById('inputSenhaLogin')
+
+let listaDeUsuarios = []
+let login
+
+
+function cadastrarUsuario() {
+
+    const nome = inputNome.value
+    const sobrenome = inputSobrenome.value
+    const cpf = inputCPF.value
+    const cnpj = inputCNPJ.value
+    const email = inputEmailCadastro.value
+    const senha = inputSenhaCadastro.value
+
+    if (nome == '') {
+        return alert('Digite um nome...')
+    }
+
+    if (sobrenome == '') {
+        return alert('Digite um sobrenome...')
+    }
+
+    if (cpf == '') {
+        return alert('Digite um cpf...')
+    }
+
+    if (email == '') {
+        return alert('Digite um E-mail...')
+    }
+
+    if (senha == '') {
+        return alert('Digite uma senha...')
+    }
+
+    listaDeUsuarios.push({
+        nome: nome,
+        sobrenome: sobrenome,
+        cpf: cpf,
+        cnpj: cnpj,
+        email: email,
+        senha: senha,
+        campanhas: [],
+    })
+
+    localStorage.setItem('usuarios', JSON.stringify(listaDeUsuarios))
+    limparCampos()
+    alert('Cadastro bem-sucedido!')
+}
+
+
+function logarUsuario() {
+    
+    const email = inputEmailLogin.value
+    const senha = inputSenhaLogin.value
+
+    if (email == '') {
+        return alert('Digite um E-mail...')
+    }
+
+    if (senha == '') {
+        return alert('Digite uma senha...')
+    }
+
+    listaDeUsuarios.forEach((usuario, index) => {
+
+        if (usuario.email == email && usuario.senha == senha) {
+            localStorage.setItem('login', JSON.stringify({logado: true, index: index}))
+            return window.location.href = 'area-logada.html'
+        }
+    })
+
+    return alert('E-mail ou senha inválidas')
+}
+
+
+function deslogarUsuario() {
+
+    localStorage.setItem('login', JSON.stringify({logado: false}))
+    return window.location.href = 'index.html'
+}
+
+
+function limparCampos() {
+
+    inputNome.value = ''
+    inputSobrenome.value = ''
+    inputCPF.value = ''
+    inputCNPJ.value = ''
+    inputEmailCadastro.value = ''
+    inputSenhaCadastro.value = ''
+    inputEmailLogin.value = ''
+    inputSenhaLogin.value = ''
+}
+
+
+function recarregarUsuarios() {
+
+    const usuariosLocalStorage = localStorage.getItem('usuarios')
+    const logadoLocalStorafe = localStorage.getItem('login')
+
+    if (usuariosLocalStorage) {
+        listaDeUsuarios = JSON.parse(usuariosLocalStorage)
+    }
+    
+    if (logadoLocalStorafe) {
+        login = JSON.parse(logadoLocalStorafe)
+    }
+}
+
+recarregarUsuarios()
+console.log(login.logado)
+if (window.location.href.indexOf('cadastrar-usuario.html') !== -1) {
+    if (login.logado) {
+        window.location.href = 'area-logada.html'
+    }
+}
